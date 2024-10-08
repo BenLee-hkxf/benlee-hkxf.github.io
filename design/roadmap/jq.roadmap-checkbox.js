37eb692c92901dac76ab71a1ba2297ee4259123b
Ben Lee <benlee@hkxf.org>
2024-10-09 02:27:16 +0800

    修正事件綁定問題、更新json

--- design/roadmap/jq.roadmap-checkbox.js
+++ design/roadmap/jq.roadmap-checkbox.js
@@ -24,7 +24,7 @@ function cacheAndRender(jsonNode) {
     }
     nodeStat = initTree(jsonNode);
    
-    console.log('nodeStat', nodeStat);
+    // console.log('nodeStat', nodeStat);
     renderTree()
 }
 
@@ -34,9 +34,10 @@ function renderTree(){
     const ul = $('<ul>').appendTo(container);
     recursiveRenderNode(nodeStat,'').appendTo(ul);
 
-    const jsonContainer = $('.json-container').empty();
+    const jsonArea = $('.json-container textarea');
+    jsonArea.text(JSON.stringify(nodeStat))
 
-    $('<textarea class="dark w-full" rows="20" >').text(JSON.stringify(nodeStat)).appendTo(jsonContainer);
+    initFlowbite();
 }
 
 
@@ -47,13 +48,13 @@ function recursiveRenderNode(node, path){
    
     node.path = path;
     const li = renderNode(node);
-    if (node.children){
+    if (node.children && node.children.length > 0){
         const ul = $('<ul class="children">').appendTo(li);
         node.children.map((child, i) => {
-            //child.parent = node;
-            if (child.path){
-                parentTree[child.path] = node;
-            }
+
+            // if (child.path){
+            //     parentTree[child.path] = node;
+            // }
            
             recursiveRenderNode(child, `${path}.${i}`).appendTo(ul);
         });
@@ -72,7 +73,12 @@ function renderNode(node) {
     createCollapseButton(node, li).appendTo(el);
 
 
-    const cb = $('<input type="checkbox">').attr('name', node.path).appendTo(label);
+    const cb = $(
+        '<input type="checkbox" class="node-checked">'
+    ).attr({
+        'name': node.path, 
+        'data-path':node.path
+    }).appendTo(label);
 
     // 有子項目
     //   狀態三選一：all-true, all-false, both
@@ -98,22 +104,6 @@ function renderNode(node) {
 
 
     const title = $('<span>').text(node.title).appendTo(label);
-
-    // 異動之後，都要整個dom tree砍掉重新長，以免事件互相影響
-    cb.on('change', function(){
-        const checked = cb.prop('checked');
-        console.log('checked changed', node.title, checked);
-        // const some = cb.prop('indeterminate');
-
-        // node.checked = checked;
-        // node.cb = cb.prop('indeterminate')?2: cb.prop('checked')==1;
-        node.cb = checked?1:0;
-
-        loopCheckAllChildren(node, checked);
-        loopCheckAllParent(node);
-
-        saveTree();
-    });
 
     return li;
 }
@@ -145,13 +135,12 @@ $('.editor-cancel-button').on('click', function(){});
 
 function createCollapseButton(node, li){
     
-    // const toggler = $('<button>').text('toggle').appendTo(el);
     const gridCollapsed = $(
         '<div class="grid-collapsed-row flex gap-1 items-center">'
       );
 
     const editButton = $(
-        '<span class="text-blue-500 opacity-10 hover:opacity-100 transition-opacity cursor-pointer node-act-btn node-act-edit material-symbols-outlined">edit</span>'
+        '<span class="opacity-10 hover:opacity-100 transition-opacity cursor-pointer node-btn node-act-btn node-act-edit material-symbols-outlined">edit</span>'
     ).attr({
         'data-path': node.path,
         'data-modal-target':'edit-modal', 
@@ -161,17 +150,17 @@ function createCollapseButton(node, li){
     //  data-modal-target="default-modal" data-modal-toggle="default-modal"
     
     const addButton = $(
-        '<span class="text-lime-500 opacity-10 hover:opacity-100 transition-opacity cursor-pointer node-act-btn node-act-add material-symbols-outlined">add_circle</span>'
+        '<span class="opacity-10 hover:opacity-100 transition-opacity cursor-pointer node-btn node-act-btn node-act-add material-symbols-outlined">add_circle</span>'
       ).attr('data-path', node.path).appendTo(gridCollapsed);
 
     const removeButton = $(
-        '<span class="text-rose-500 opacity-10 hover:opacity-100 transition-opacity cursor-pointer node-act-btn node-act-remove material-symbols-outlined">cancel</span>'
+        '<span class="text-rose-500 opacity-10 hover:opacity-100 transition-opacity cursor-pointer node-btn node-act-btn node-act-remove material-symbols-outlined">cancel</span>'
       ).attr('data-path', node.path).appendTo(gridCollapsed);
 
 
 
     const toggler = $(
-        '<span class="collapse-toggler node-act-btn cursor-pointer material-symbols-outlined"></span>'
+        '<span class="collapse-toggler node-btn cursor-pointer material-symbols-outlined"></span>'
       );
     
     if (node.children && node.children.length > 0){
@@ -179,63 +168,49 @@ function createCollapseButton(node, li){
         toggler.text("expand_circle_down");
     }else{
         // add empty placeholder
-        $('<span class="w-[40px] node-act-btn material-symbols-outlined">').appendTo(gridCollapsed);
+        $('<span class="w-[40px] node-btn material-symbols-outlined">').appendTo(gridCollapsed);
     }
     
     toggler.on("click", function () {
-        // node parent li
         li.toggleClass("collapsed");
         node.collapsed = !node.collapsed;
-        // el.toggleClass("collapsed");
     });
 
     return gridCollapsed;
 }
 
-
-function loopCheckAllChildren(node, checked){
+// 強制更新勾選狀態至所有子項目
+function loopApplyChecked(node, checked){
     if (node.children ){
         node.children.forEach((child) => {
-            // child.checked = checked;
-            // delete child.both;
             child.cb = checked?1:0;
-            loopCheckAllChildren(child, checked);
+            loopApplyChecked(child, checked);
         });
     }
 }
 
 
-
-function loopCheckAllParent(node){
+// 遞迴更新 parent 的勾選狀態
+function loopDetermineParentChecked(node){
 
     if (parentTree[node.path]){
         const parent = parentTree[node.path];
         const children = parent.children;
-        // parent.checked = children.every((child) => child.checked);
-        // parent.both = children && children.length>0? children.some((child) => child.checked) && children.some((child) => !child.checked) : false;
-        parent.cb = children.every((child) => child.cb==1)?1: children.some((child) => child.cb != 0) ? 2 : 0;
-        loopCheckAllParent(parent);
+        // 這邊以個小 bug，若 children 全都沒有 cb 屬性，第2個運算式會成立 => parent 變成部分選取
+        parent.cb = children.every((child) => child.cb==1)?1: 
+        children.some((child) => child.cb) && children.some((child) => child.cb != 0) ? 2 : 
+            0;
+        loopDetermineParentChecked(parent);
     }
 }
 
-// 首次 render 之前，先把每一層級的 node 預先設定好 both 值
+
 function initTree(node){
+    const jsonContainer = $('.json-container').empty();
+    $('<textarea class="dark w-full" rows="20" >').appendTo(jsonContainer);
 
     // 由上而下，調整 both 與 checked 狀態
     loopChildrenStatus(node);
-
-
-        
-    $('.editor-save-button').on('click', function(){
-        const path = $('.node-path').val();
-        const title = $('.node-name').val();
-        
-        const node = loopFindNode(nodeStat, path);
-        node.title = title;
-
-        saveTree();
-        $('.node-path').val('');
-    });
 
     return node;
 }
@@ -244,9 +219,12 @@ function loopChildrenStatus(node){
 
     if (!node.children || node.children.length == 0){
         delete node.children;
-        loopCheckAllParent(node);
+        loopDetermineParentChecked(node);
     } else {
         node.children.forEach((child) => {
+            if (child.path){
+                parentTree[child.path] = node;
+            }
 
             loopChildrenStatus(child);
         });
@@ -254,9 +232,41 @@ function loopChildrenStatus(node){
 
 }
 
+
+    // node-checked
+    // 異動之後，都要整個dom tree砍掉重新長，以免事件互相影響
+    $(document).on('change', 'input[type=checkbox].node-checked', function(){
+        const cb = $(this);
+        const node = loopFindNode(nodeStat, cb.attr('data-path'));
+        const checked = cb.prop('checked');
+        // console.log('checked changed', node.title, checked);
+        node.cb = checked?1:0;
+
+        loopApplyChecked(node, checked);
+        loopDetermineParentChecked(node);
+
+        saveTree();
+    });
+
+
+$(document).on('focus', 'input.node-name', function(){
+    $(this).select();
+});
+
+$(document).on('click', '.editor-save-button', function(){
+    const path = $('.node-path').val();
+    const title = $('.node-name').val();
+    
+    const node = loopFindNode(nodeStat, path);
+    node.title = title;
+
+    saveTree();
+    $('.node-path').val('');
+});
+
 $(document).on('click', '.node-act-edit', function(){
-    let modal = document.getElementById('edit-modal');
-    // modal.style.display = 'block';
+    // let modal = $('.edit-modal');
+    // modal.toggleClass('hidden');    // modal.style.display = 'block';
 
     const path = $(this).attr('data-path');
     const node = loopFindNode(nodeStat, path);
@@ -270,8 +280,6 @@ $(document).on('click', '.node-act-add', function(){
 
     const targetNode = loopFindNode(nodeStat, path);
 
-
-
     const newNode = {
         title: 'node '+ Math.ceil(Math.random()*100000000),
         cb: 0,
@@ -283,18 +291,21 @@ $(document).on('click', '.node-act-add', function(){
         targetNode.children = [newNode];
     }
 
-    newNode.path = path + '.' + targetNode.children.length;
+    newNode.path = [path, targetNode.children.length-1].join('.');
     parentTree[newNode.path] = targetNode;
-    loopCheckAllParent(newNode);
+    loopDetermineParentChecked(newNode);
     saveTree();
 });
 
 $(document).on('click', '.node-act-remove', function(){  
     const path = $(this).attr('data-path');
     
-
     const found = loopFindNode(nodeStat, path);
     if (found){
+        if (found.children&&found.children.length>0 
+            && !confirm(`請再次確認，刪除 "${found.title}" 及其下${found.children.length}個節點?`)){
+            return;
+        }
         const parent = parentTree[found.path];
 
         if (parent){
@@ -306,9 +317,9 @@ $(document).on('click', '.node-act-remove', function(){
 
                 // 移除之後，要從 sibling 角度查一次，更新 parent 的狀態
                 if (foundParent.children.length > 0){
-                    loopCheckAllParent(foundParent.children[0]);
+                    loopDetermineParentChecked(foundParent.children[0]);
                 }else{
-                    loopCheckAllParent(parent);
+                    loopDetermineParentChecked(parent);
                 }
 
                 saveTree();
@@ -318,3 +329,13 @@ $(document).on('click', '.node-act-remove', function(){
 
     //parentTree[node.path].children = parentTree[node.path].children.filter((child) => child !== node);
   });
+
+(function($){
+    $(document).ready(function(){
+        $(document.body).toggleClass('editor-mode', window.location.hash=='#editor-mode');
+    
+        // initFlowbite 時，如果 data-modal-target 的元素還沒加入，FlowBite 會報錯
+        initFlowbite();
+        //console.log(window.location.hash)
+    })
+})(jQuery);
